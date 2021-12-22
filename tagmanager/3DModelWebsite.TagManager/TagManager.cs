39cb76b742dd161746e7d589119b319c0902a7d3
Pierre-Marie Aubineau <pm@cataire.fr>
2021-12-22 14:10:19 +0100

    Modification du namespace de TagManager.cs

--- tagmanager/3DModelWebsite.TagManager/TagManager.cs
+++ tagmanager/3DModelWebsite.TagManager/TagManager.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _3DModelWebsite.TagManager
+namespace The3DModelWebsite.Tags
 {
+    /// <summary>
+    /// Operations of Add, Delete, Update, and list for the application tags.
+    /// </summary>
     internal class TagManager
     {
     }
