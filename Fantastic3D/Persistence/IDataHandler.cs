aa85856e98b06f36272eee973973b8395ee50e19
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-05 14:14:16 +0100

    Création du projet gérant la persistance des données en local

---
+++ Fantastic3D/Persistence/IDataHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantastic3D.Persistence
+{
+    /// <summary>
+    /// Les éléments de cette interface peuvent
+    /// </summary>
+    internal interface IDataHandler
+    {
+        public void SaveData(object obj);
+        public object GetData();
+    }
+}
---
+++ Fantastic3D/Persistence/XMLDataHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Fantastic3D.Persistence
+{
+    public class XMLDataHandler : IDataHandler
+    {
+        public object GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveData(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
