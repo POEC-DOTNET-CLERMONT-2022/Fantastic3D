5c4ba70b94536d9da54348b2c253e7f7a54f761f
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-05 15:52:49 +0100

    Création d'un programme de test console pour des tests rapides de nouvelles fonctions

---
+++ Fantastic3D/DirtyConsoleTests/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fantastic3D.Models;
+using Fantastic3D.Persistence;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Cette application permet de tester ce qu'on a implémenté hors de tout contexte, pour nous assurer que les éléments sont solides et n'ont pas trop de dépendance.");
+
+var dataHandler = new XMLDataHandler();
+
+var tagTypeOne = new TagType("Thématique", true, false);
+
+var maListe = new List<Tag>();
+maListe.Add(new Tag("Horreur", tagTypeOne));
+maListe.Add(new Tag("Western", tagTypeOne));
+maListe.Add(new Tag("Moderne", tagTypeOne));
+maListe.Add(new Tag("Sci-fi", tagTypeOne));
+maListe.Add(new Tag("Antique", tagTypeOne));
+
+dataHandler.SaveData("tags", maListe);
+
+Console.WriteLine("La Sauvegarde est faite dans " + Environment.CurrentDirectory);
+
+var myLoadedList = (List<Tag>)dataHandler.LoadData("tags");
+
+Console.WriteLine("Lecture de la liste récupérée");
+foreach(var tag in myLoadedList)
+{
+    Console.WriteLine(tag.ToString());
+}
+
+Console.ReadLine();
