0ba37c5e29bc2e7d872ba8bf8372f45c3142cb88
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-05 17:59:54 +0100

    Ajout du service WCF (auto généré)

---
+++ Fantastic3D/FantasticService.WCFApp/WebUserService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Fantastic3D.FantasticService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    /// <summary>
+    /// Classe du Service capable de transmettre des informations d'utilisateurs du site Fantastic3D.
+    /// </summary>
+    public class WebUserService : IWebUserService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
