c3e869a274c75c2f6c747d221f720d26325db324
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-05 12:29:52 +0100

    Ajout du projet GUI en WPF pour l'application (v1)

---
+++ Fantastic3D/Fantastic3D.WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Fantastic3D.GUI.SectionControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fantastic3D.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ModelViewerControl modelViewer { get; set; } = new();
+        private ModelListControl modelList { get; set; } = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowContent(UserControl userControl)
+        {
+            MainContent.Content = userControl;
+        }
+
+        private void TopMenuControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuHomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuModelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            ShowContent(modelList);
+            ShowContent(modelViewer);
+        }
+    }
+}
---
+++ Fantastic3D/Fantastic3D.WPFApp/SectionControls/ModelViewerControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Fantastic3D.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fantastic3D.GUI
+{
+    /// <summary>
+    /// Logique d'interaction pour ModelViewerControl.xaml
+    /// </summary>
+    public partial class ModelViewerControl : UserControl
+    {
+        public List<Asset> ModelsToValidate { get; set; }
+        public ModelViewerControl()
+        {
+            InitializeComponent();
+
+            ModelsToValidate = new();
+            // TODO: Faire la population des données avec des données correspondant aux modèles.
+            //ModelsToValidate.Add(new Asset("Gaufrier", 52));
+            //ModelsToValidate.Add(new Asset("Voiture de sport", 21));
+            //ModelsToValidate.Add(new Asset("Pot de cookies", 500));
+            //ModelsToValidate.Add(new Asset("Zombie Baron Samedi", 58));
+
+            NextModelsList.ItemsSource = ModelsToValidate;
+            //NextModelsList.UpdateLayout();
+            //ModelsToValidate[0].Item1
+        }
+
+    }
+}
