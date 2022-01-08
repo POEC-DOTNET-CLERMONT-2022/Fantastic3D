98a5831705a0e3f7f1fb42468a09ce52a628f7b8
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-08 12:10:01 +0100

    AppConsole : initialisation du TagManager avec la persistance plutôt que des données en dur dans le code.

--- Fantastic3D/TagManager.ConsoleApp/Program.cs
+++ Fantastic3D/TagManager.ConsoleApp/Program.cs
@@ -7,17 +7,21 @@ Console.WriteLine("Hello, World!");
 
 
 // TODO : les types de tag sont codés en dur. Faire un import depuis persistance.
-var allTagTypes = new List<TagType>()
-{
-    new TagType("Thématique", true, false),
-    new TagType("Catégories", true, false),
-    new TagType("Style", false, false),
-    new TagType("Format", false, isOnlyOne:true),
-    new TagType("Licence", false, isOnlyOne:true),
-    new TagType("Capacités", false, false),
-};
+//var allTagTypes = new List<TagType>()
+//{
+//    new TagType("Thématique", true, false),
+//    new TagType("Catégories", true, false),
+//    new TagType("Style", false, false),
+//    new TagType("Format", false, isOnlyOne:true),
+//    new TagType("Licence", false, isOnlyOne:true),
+//    new TagType("Capacités", false, false),
+//};
 
-var tagManager = new TagManager(new ConsoleReader(), new ConsoleWritter(), new XMLDataHandler(), allTagTypes);
+var allTagTypes = new List<TagType>();
+var dummyDataMaker = new DummyDataHandler<TagType>();
+dummyDataMaker.LoadData(allTagTypes);
+
+var tagManager = new TagManager(new ConsoleReader(), new ConsoleWritter(), new DummyDataHandler<Tag>(), allTagTypes);
 
 bool WeContinue = true;
 
