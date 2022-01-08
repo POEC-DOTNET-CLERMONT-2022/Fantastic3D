7e4eb49960a47a7bf3a2ff2e3b17af455b765b21
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-08 11:54:44 +0100

    Persistence : modification de l'interface IDataHandler pour la rendre générique, compatible uniquement avec les IPersistable

--- Fantastic3D/Persistence/IDataHandler.cs
+++ Fantastic3D/Persistence/IDataHandler.cs
@@ -1,21 +1,24 @@
-﻿using Fantastic3D.Models;
+﻿
 
 namespace Fantastic3D.Persistence
 {
     /// <summary>
-    /// Elements of this interface can Load and Save Data of various formats
+    /// Elements of this interface can Load and Save Data implementing IPeristable
     /// </summary>
-    public interface IDataHandler
+    public interface IDataHandler<T>
+        where T : Models.IPersistable
     {
         /// <summary>
-        /// Tries to save the data using identifier dataIdentifier.
+        /// 
         /// </summary>
-        public void SaveData(string dataIdentifier, object obj);
+        /// <param name="listToSave">Data which will be saved.</param>
+        public void SaveData(List<T> listToSave);
 
         /// <summary>
-        /// Tries to get the data using the identifier dataIdentifier.
-        /// Returns an empty value if no file exists.
+        /// Loads data for the corresponding class into loadedList.
+        /// If data exists for a given instance, it will be overwritten.
         /// </summary>
-        public object LoadData(string dataIdentifier);
+        /// <param name="loadedList">List in which the data will be populated.</param>
+        public void LoadData(List<T> loadedList);
     }
 }
