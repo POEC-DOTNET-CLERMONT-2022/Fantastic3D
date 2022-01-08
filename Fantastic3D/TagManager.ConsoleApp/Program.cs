d8251d96bc9597410eed6f413d2fae7480b06ca8
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-08 12:22:55 +0100

    App Console : tagManager initialisé avec sauvegarde dans XML + suppression des données en dur commentées.

--- Fantastic3D/TagManager.ConsoleApp/Program.cs
+++ Fantastic3D/TagManager.ConsoleApp/Program.cs
@@ -3,23 +3,11 @@ using Fantastic3D.Models;
 using Fantastic3D.Persistence;
 using Fantastic3D.Tags;
 
-
-// TODO : les types de tag sont codés en dur. Faire un import depuis persistance.
-//var allTagTypes = new List<TagType>()
-//{
-//    new TagType("Thématique", true, false),
-//    new TagType("Catégories", true, false),
-//    new TagType("Style", false, false),
-//    new TagType("Format", false, isOnlyOne:true),
-//    new TagType("Licence", false, isOnlyOne:true),
-//    new TagType("Capacités", false, false),
-//};
-
 var allTagTypes = new List<TagType>();
 var dummyDataMaker = new DummyDataHandler<TagType>();
 dummyDataMaker.LoadData(allTagTypes);
 
-var tagManager = new TagManager(new ConsoleReader(), new ConsoleWritter(), new DummyDataHandler<Tag>(), allTagTypes);
+var tagManager = new TagManager(new ConsoleReader(), new ConsoleWritter(), new XMLDataHandler<Tag>(), allTagTypes);
 
 var mainMenu = new Menu(new ConsoleReader(), new ConsoleWritter(), tagManager, allTagTypes);
 
