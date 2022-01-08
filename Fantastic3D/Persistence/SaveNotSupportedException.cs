a994efcec5483944f86682a48e25d695d9e9473f
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-08 11:53:37 +0100

    Persistence : création Exception "SaveNotSupported"

---
+++ Fantastic3D/Persistence/SaveNotSupportedException.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace Fantastic3D.Persistence
+{
+    /// <summary>
+    /// Exception raised when the handler can only load, but not save.
+    /// </summary>
+    [Serializable]
+    internal class SaveNotSupportedException : Exception
+    {
+        public SaveNotSupportedException()
+        {
+        }
+
+        public SaveNotSupportedException(string? message) : base(message)
+        {
+        }
+
+        public SaveNotSupportedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected SaveNotSupportedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
