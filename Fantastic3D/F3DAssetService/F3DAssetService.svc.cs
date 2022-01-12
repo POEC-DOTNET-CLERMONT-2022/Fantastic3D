69cbee7c4acb45501f812d014eb4cd1c1cdb802a
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-12 22:52:37 +0100

    (add #7) Service WCF consommant un jeu de données et le diffusant via 3 contrats.

---
+++ Fantastic3D/F3DAssetService/F3DAssetService.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Fantastic3D.Legacy.Dtos;
+
+namespace Fantastic3D.Legacy.Services
+{ 
+     // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class F3DAssetService : IAssetService
+    {
+        IDtoImporter<AssetDto> _dataImporter = new DummyDtoImporter();
+        List<AssetDto> _importedDtos = new List<AssetDto>();
+
+        public F3DAssetService()
+        {
+            _dataImporter.ImportData(_importedDtos);
+        }
+
+        public List<string> GetList(int offset = 0)
+        {
+            return _importedDtos.Select(asset => asset.Name).ToList();
+            // TODO : [Service / GetList()] : coder l'offset et la limite de 10
+        }
+
+        AssetDto IAssetService.SearchAssetDto(string modelName)
+        {
+            try
+            {
+                var assetDto = _importedDtos.Single(asset => asset.Name.Contains(modelName));
+                return assetDto;
+            }
+            catch (InvalidOperationException ioe)
+            {
+                throw new InvalidOperationException("Several models found containing those characters : " + modelName, ioe);
+            }
+        }
+
+        public AssetDto GetAssetDto(int modelId)
+        {
+            return _importedDtos.Single(asset => asset.Id == modelId);
+        }
+    }
+}
---
+++ Fantastic3D/F3DAssetService/IAssetService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Fantastic3D.Legacy.Dtos;
+
+namespace Fantastic3D.Legacy.Services
+{
+    [ServiceContract]
+    public interface IAssetService
+    {
+
+        /// <summary>
+        /// Returns the names of the 10 first models found,.
+        /// </summary>
+        /// <param name="offset">Starts the resulting list with an offset.</param>
+        /// <returns></returns>
+        [OperationContract]
+        List<string> GetList(int offset = 0);
+
+        /// <summary>
+        /// Get information about a model from its name.
+        /// </summary>
+        [OperationContract]
+        AssetDto SearchAssetDto(string modelName);
+        /// <summary>
+        /// Get information about a model from its ID.
+        /// </summary>
+        [OperationContract]
+        AssetDto GetAssetDto(int modelId);
+    }
+}
