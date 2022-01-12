25e17281a6f04328340433106b7f27070c65afed
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-12 22:49:13 +0100

    (add #8) App WPF "Reader" capable de consommer notre service WCF.

---
+++ Fantastic3D/ExternalDataReader.WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Fantastic3D.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExternalDataReader.WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new AssetServiceClient();
+                RequestOutput.Text = (await client.SearchAssetDtoAsync(UserInput.Text)).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Modèle non trouvé.");
+            }
+        }
+
+        private async void ListButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new AssetServiceClient();
+                RequestOutput.Text = String.Join(Environment.NewLine, await client.GetListAsync(0));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connexion dysfonctionnelle ou base vide.");
+            }
+        }
+    }
+}
