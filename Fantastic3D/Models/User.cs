ffa356b72807984fe60c310ce1484b41cf8b0700
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-14 18:17:33 +0100

    (update) Mise à jour du modèle User (décorateurs pour EF)

--- Fantastic3D/Models/User.cs
+++ Fantastic3D/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace Fantastic3D.Models
 {
@@ -7,43 +9,46 @@ namespace Fantastic3D.Models
     /// </summary>
     public enum UserRole { Admin, Premium, Basic };
 
-    [DataContract]
+
+    [DataContract, Table("users")]
     public class User : IPersistable
     {
-        [DataMember]
-        private Guid _id;
-        [DataMember]
+        [Key, DataMember, Column("id")]
+        public Guid Id { get; set; }
+        [DataMember, Column("username")]
         private string _username;
-        [DataMember]
+        [DataMember, Column("first_name")]
         private string _firstName;
-        [DataMember]
+        [DataMember, Column("last_name")]
         private string _lastName;
-        [DataMember]
+        [DataMember, Column("email")]
         private string _email;
-        [DataMember]
+        [DataMember, Column("password")]
         private string _password;
-        [DataMember]
+        [DataMember, Column("hash_salt")]
         private string _hashSalt;
-        [DataMember]
-        private string _billingAdress;
-        [DataMember]
+        [DataMember, Column("billing_address")]
+        private string _billingAddress;
+        [DataMember, Column("role")]
         private UserRole _role;
 
-        public User(Guid id, string username, string firstName, string lastName, string email, string password, string hashSalt, string billingAdress, UserRole role)
+        public User() {}
+
+        public User(Guid id, string username, string firstName, string lastName, string email, string password, string billingAdress, UserRole role)
         {
-            _id = id;
+            Id = id;
             _username = username;
             _firstName = firstName;
             _lastName = lastName;
             _email = email;
             _password = Utilities.PasswordHash(password, out _hashSalt);
-            _billingAdress = billingAdress;
+            _billingAddress = billingAdress;
             _role = role;
         }
         
         public void SetNewPassword(string plainPassword)
         {
-            _password = Utilities.PasswordHash(password, out _hashSalt);
+            _password = Utilities.PasswordHash(plainPassword, out _hashSalt);
         }
 
         public bool MatchPassword(string plainPassword)
@@ -53,7 +58,7 @@ namespace Fantastic3D.Models
 
         public override string ToString()
         {
-            return $"Email : {_email}, Pass : {_password}, BillingAdresse : {_billingAdress}, Role : {_role})";
+            return $"Email : {_email}, Pass : {_password}, BillingAdresse : {_billingAddress}, Role : {_role})";
         }
     }
 }
