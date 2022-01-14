ef604c83eb566b7f8eedf693937fc8d81e7636dc
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-14 18:15:58 +0100

    (add) Création d'un DbContext pour la Persistance en Database

---
+++ Fantastic3D/Persistence/LocalDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Fantastic3D.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fantastic3D.Persistence
+{
+    public class LocalDbContext : DbContext
+    {
+        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        //public override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    EntityTypeBuilder<User> userCatalog = modelBuilder.Entity<User>().ToTable("User");
+        //    userCatalog.HasKey(u => u._id);
+        //}
+            
+    }
+}
