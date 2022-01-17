3c201d23c10dcc76f64795845b4de18c386bd529
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-17 21:58:19 +0100

    (del) fichiers en doublon

--- Fantastic3D/Entities/IPersistable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fantastic3D.Entities
-{
-    /// <summary>
-    /// Interface for classes that can be saved into persistence.
-    /// </summary>
-    public interface IPersistable
-    {
-    }
-}
