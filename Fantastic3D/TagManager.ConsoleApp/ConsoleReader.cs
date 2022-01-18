57b63fa1f004ca34e46ddb97a3cc32db0f0cd69c
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-18 19:25:53 +0100

    (upd) ConsoleReader et ConsoleWriter ajoutés au namespace "Tags"

--- Fantastic3D/TagManager.ConsoleApp/ConsoleReader.cs
+++ Fantastic3D/TagManager.ConsoleApp/ConsoleReader.cs
@@ -1,55 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Fantastic3D.Persistence.Entities;
-using Fantastic3D.Tags;
-
-internal class ConsoleReader : IReader
+﻿namespace Fantastic3D.Tags
 {
-    public IWriter Writer { get; set; }
-    
-    public T GetElementFromList<T>(List<T> choices, string prompt)
+    internal class ConsoleReader : IReader
     {
-        int choiceIndex = 1;
-        foreach (var item in choices)
-        {
-            Writer.Display($"{choiceIndex++} : {item}");
-        }
-        int choiceWithOffset = this.ReadId(1, choiceIndex - 1);
+        public IWriter Writer { get; set; }
 
-        return choices[choiceWithOffset - 1];
-    }
-
-    public int ReadId(int lowerBound, int higherBound)
-    {
-        bool valueIsCorrect = false;
-        int outputValue = 0;
-        //int cursorPosition = Console.GetCursorPosition();
-        while(!valueIsCorrect)
+        public T GetElementFromList<T>(List<T> choices, string prompt)
         {
-            var userInput = Console.ReadLine();
-            if(int.TryParse(userInput, out outputValue))
+            int choiceIndex = 1;
+            foreach (var item in choices)
             {
-                if(outputValue >= lowerBound && outputValue <= higherBound)
+                Writer.Display($"{choiceIndex++} : {item}");
+            }
+            int choiceWithOffset = this.ReadId(1, choiceIndex - 1);
+
+            return choices[choiceWithOffset - 1];
+        }
+
+        public int ReadId(int lowerBound, int higherBound)
+        {
+            bool valueIsCorrect = false;
+            int outputValue = 0;
+            //int cursorPosition = Console.GetCursorPosition();
+            while (!valueIsCorrect)
+            {
+                var userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out outputValue))
                 {
-                    valueIsCorrect = true;
+                    if (outputValue >= lowerBound && outputValue <= higherBound)
+                    {
+                        valueIsCorrect = true;
+                    }
                 }
             }
+            return outputValue;
         }
-        return outputValue;
-    }
 
-    public string ReadText()
-    {
-        // TODO : une boucle, tant que c'est "null", refaire un readline
-        bool stringIsCorrect = false;
-
-        var InputUserString = Console.ReadLine();
-        while (!stringIsCorrect)
+        public string ReadText()
         {
-            if (InputUserString != null)
+            // TODO : une boucle, tant que c'est "null", refaire un readline
+            bool stringIsCorrect = false;
+
+            var InputUserString = Console.ReadLine();
+            while (!stringIsCorrect)
             {
-                stringIsCorrect = true;
+                if (InputUserString != null)
+                {
+                    stringIsCorrect = true;
+                }
             }
+            return InputUserString;
         }
-        return InputUserString;
     }
 }
--- Fantastic3D/TagManager.ConsoleApp/ConsoleWriter.cs
+++ Fantastic3D/TagManager.ConsoleApp/ConsoleWriter.cs
@@ -1,31 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Fantastic3D.Persistence.Entities;
-using Fantastic3D.Tags;
-
-internal class ConsoleWriter : IWriter
+﻿namespace Fantastic3D.Tags
 {
-    private string _appTitle = "";
-    private string _menuSectionTitle = "";
-    public void Display(string text)
+    internal class ConsoleWriter : IWriter
     {
-        Console.WriteLine(text);
-    }
+        private string _appTitle = "";
+        private string _menuSectionTitle = "";
+        public void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
 
-    public void SetAppTitle(string title)
-    {
-        _appTitle = title;
-        Console.Clear();
-        Console.WriteLine(_appTitle);
-    }
+        public void SetAppTitle(string title)
+        {
+            _appTitle = title;
+            Console.Clear();
+            Console.WriteLine(_appTitle);
+        }
 
-    public void SetMenuHeader(string sectionHeader)
-    {
-        _menuSectionTitle = sectionHeader;
-        Console.WriteLine(_menuSectionTitle);
-    }
+        public void SetMenuHeader(string sectionHeader)
+        {
+            _menuSectionTitle = sectionHeader;
+            Console.WriteLine(_menuSectionTitle);
+        }
 
-    public void DisplayTag(TagEntity tag)
-    {
-        Console.WriteLine(tag);
+        public void DisplayTag(Fantastic3D.Persistence.Entities.TagEntity tag)
+        {
+            Console.WriteLine(tag);
+        }
     }
 }
