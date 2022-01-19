0c761873b2e05018146eb103f1103afc50487bc5
Sylvain125 <kenjii@hotmail.fr>
2022-01-19 11:53:30 +0100

    (Add) des profils pour automapper

---
+++ Fantastic3D/Fantastic3D.AppModels/UserModelProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Fantastic3D.Dto;
+
+namespace Fantastic3D.AppModels
+{
+    public class UserModelProfile : Profile 
+    {
+        public UserModelProfile()
+        {
+            CreateMap<UserDto, User>();
+
+            CreateMap<TagDto, Tag>();
+
+            CreateMap<TagTypeDto, TagType>();
+
+            CreateMap<AssetDto, Asset>();
+
+            CreateMap<OrderDto, Order>();
+
+            CreateMap<PurchaseDto, Purchase>();
+
+            CreateMap<ReviewDto, Review>();
+
+
+        }
+    }
+}
---
+++ Fantastic3D/Persistence/Entities/UserDtoProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Fantastic3D.Dto;
+using Fantastic3D.Persistence.Entities;
+
+namespace Fantastic3D.Persistence.Entities
+{
+    public class UserDtoProfile : Profile
+    {
+        public UserDtoProfile()
+        {
+            CreateMap<UserEntity, UserDto>();
+
+            CreateMap<TagEntity, TagDto>();
+
+            CreateMap<TagTypeEntity, TagTypeDto>();
+
+            CreateMap<AssetEntity, AssetDto>();
+
+            CreateMap<OrderEntity, OrderDto>();
+
+            CreateMap<PurchaseEntity, PurchaseDto>();
+
+            CreateMap<ReviewEntity, ReviewDto>();
+
+
+        }
+    }
+}
