c7a90a3fc948fc314e3fdced79d9e62aa3fc08fb
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-20 09:20:29 +0100

    (add) Création de la classe IWithId, destinée aux objets amenés à transiter via l'API.

---
+++ Fantastic3D/Fantastic3D.Dto/IWithId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantastic3D.Dto
+{
+    /// <summary>
+    /// Defines classes that contains an Id.
+    /// </summary>
+    internal interface IWithId
+    {
+        public int Id { get; set; }
+    }
+}
