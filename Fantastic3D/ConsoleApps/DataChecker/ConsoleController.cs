59dfbfedd24f0d3dd1dbb7b1704b8fa1d9eb754b
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-23 18:15:41 +0100

    (add) DataChecker (remplace l'application console de tests)

---
+++ Fantastic3D/ConsoleApps/DataChecker/ConsoleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DataChecker
+{
+    internal class ConsoleController
+    {
+        private string _appTitle;
+
+        public ConsoleController(string appTitle)
+        {
+            _appTitle = appTitle;
+        }
+
+        public void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void ClearAndShowTitle()
+        {
+            Console.Clear();
+            var previousForegroundColor = Console.ForegroundColor;
+            var previousBackgroundColor = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"***** {_appTitle} *****");
+            Console.ForegroundColor = previousForegroundColor;
+            Console.BackgroundColor = previousBackgroundColor;
+        }
+
+    }
+}
---
+++ Fantastic3D/ConsoleApps/DataChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using App.DataChecker;
+using Fantastic3D.Dto;
+using Fantastic3D.Persistence;
+using Fantastic3D.Persistence.Entities;
+
+#region Xml Data
+const string dataPathFromConsoleApp = @"..\..\..\..\..\Data\";
+var xmlDataHandler = new XmlDataHandler<TagTypeEntity>(dataPathFromConsoleApp);
+
+#endregion
+
+var console = new ConsoleController("Fantastic3D Data Checker");
+console.ClearAndShowTitle();
+
+console.Display("Application permettant la vérification des données et leur passage d'un système de données à un autre.");
+console.Display("La Sauvegarde XML est faite dans " + xmlDataHandler.GetPath);
+
+console.Display("Lecture de la liste récupérée (Dummy)");
+
+List<Type> typesToFetch = new List<Type> { typeof(UserDto), typeof(TagTypeDto), typeof(TagDto), typeof(AssetDto), typeof(PurchaseDto), typeof(OrderDto), typeof(ReviewDto), };
+var dummyDataHandler = new DummyDataHandler<TagTypeEntity>();
+var dummyLoadedList = new List<TagTypeEntity>();
+dummyDataHandler.LoadData(dummyLoadedList);
+foreach (var item in dummyLoadedList)
+{
+    console.Display(item.ToString());
+}
+
+console.Display("Lecture de la liste récupérée (Xml)");
+var xmlLoadedList = new List<TagTypeEntity>();
+xmlDataHandler.LoadData(xmlLoadedList);
+foreach (var item in xmlLoadedList)
+{
+    console.Display(item.ToString());
+}
+Console.ReadLine();
