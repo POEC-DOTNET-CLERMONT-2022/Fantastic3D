d2f8e2edd31a2f31611d304235abe5e9376a7a1d
Sylvain125 <kenjii@hotmail.fr>
2022-01-27 13:50:22 +0100

    creation de la user liste pour les binding

---
+++ Fantastic3D/Fantastic3D.AppModels/UserList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantastic3D.AppModels
+{
+    public class UserList : ObservableModel
+    {
+        private ObservableCollection<User> users;
+
+        private User currentUser;
+        public User CurrentUser
+        {
+            get { return currentUser; }
+            set
+            {
+                if (currentUser != value)
+                {
+                    currentUser = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<User> Users
+        {
+            get { return users; }
+            set
+            {
+                if (users != value)
+                {
+                    users = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+
+    }
+}
