2eec8882689f6512607ca57e2beec20df404b2c2
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-27 18:42:34 +0100

    (add) Ajout système de navigation entre les vues pour l'app WPF.

---
+++ Fantastic3D/Fantastic3D.WPFApp/Utilities/INavigator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Fantastic3D.GUI.Utilities
+{
+    internal interface INavigator
+    {
+        ContentControl CurrentViewControl { get; set; }
+        bool CanGoBack { get; }
+
+        void RegisterViews(ICollection<Control> views);
+
+        void NavigateTo(Type type);
+
+    }
+}
---
+++ Fantastic3D/Fantastic3D.WPFApp/Utilities/Navigator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Fantastic3D.AppModels;
+
+namespace Fantastic3D.GUI.Utilities
+{
+    /// <summary>
+    /// Allows the navigation
+    /// </summary>
+    internal class Navigator : ObservableModel, INavigator
+    {
+        private List<Control> _availableViews { get; set; } = new List<Control>();
+        private Stack<Control> _previousViews { get; set; } = new Stack<Control>();
+        private ContentControl _currentViewControl = new ContentControl();
+        public ContentControl CurrentViewControl
+        {
+            get => _currentViewControl;
+            set
+            {
+                _currentViewControl = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+        public bool CanGoBack { get => _previousViews.Any(); }
+
+        public void NavigateTo(Type type)
+        {
+            if (CurrentViewControl == null) return;
+            if (CurrentViewControl.Content != null)
+            {
+                _previousViews.Push((Control)CurrentViewControl.Content);
+            }
+            var newView = _availableViews.SingleOrDefault(view => view.GetType() == type);
+            if (newView == null) return;
+            _currentViewControl.Content = newView;
+        }
+
+        public void RegisterViews(ICollection<Control> views)
+        {
+            _availableViews.AddRange(views);
+        }
+
+        public bool TryGoBack()
+        {   
+            if(CanGoBack || CurrentViewControl == null)
+                return false;
+            CurrentViewControl.Content = _previousViews.Pop();
+                return true;
+        }
+    }
+}
