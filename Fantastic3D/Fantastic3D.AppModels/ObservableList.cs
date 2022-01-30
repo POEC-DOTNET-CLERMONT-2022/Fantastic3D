0ea3576deb29a8f88999b64e6e0ef6a7cd88398d
Pierre-Marie Aubineau <pm@cataire.fr>
2022-01-30 19:08:14 +0100

    (add) Type générique pour les listes présentées dans l'application WPF

---
+++ Fantastic3D/Fantastic3D.AppModels/ObservableList.cs
@@ -0,0 +1,42 @@
+﻿using Fantastic3D.DataManager;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantastic3D.AppModels
+{
+    public  class ObservableList<T> : ObservableModel
+        where T : IManageable, new()
+    {
+        private ObservableCollection<T> items;
+
+        private T currentItem;
+        public T CurrentItem
+        {
+            get { return currentItem; }
+            set
+            {
+                if (!currentItem.Equals(value))
+                {
+                    currentItem = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<T> Items
+        {
+            get { return items; }
+            set
+            {
+                if (items != value)
+                {
+                    items = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
