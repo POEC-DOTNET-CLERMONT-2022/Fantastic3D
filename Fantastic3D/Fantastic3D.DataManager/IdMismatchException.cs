6202a8a2d8867010305106ebf867eaef7f0921be
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-10 16:42:56 +0100

    (add) Exception levée lors d'une incohérence entre l'Id dans l'URL et l'Id de l'objet Json lors d'une mise à jour.

---
+++ Fantastic3D/Fantastic3D.DataManager/IdMismatchException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Fantastic3D.DataManager
+{
+    [Serializable]
+    public class IdMismatchException : Exception
+    {
+        public IdMismatchException()
+        {
+        }
+
+        public IdMismatchException(string? message) : base(message)
+        {
+        }
+
+        public IdMismatchException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected IdMismatchException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
