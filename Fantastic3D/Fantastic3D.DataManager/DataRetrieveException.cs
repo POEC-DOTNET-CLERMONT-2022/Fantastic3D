1373cc3218733322ed48f86f8116d3a547802bd1
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-11 11:23:30 +0100

    (add) Exceptions pour les DataManagers

---
+++ Fantastic3D/Fantastic3D.DataManager/DataRetrieveException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fantastic3D.DataManager
+{
+    /// <summary>
+    /// Represents errors that occur when retrieving data through a DataManager.
+    /// </summary>
+    [Serializable]
+    public class DataRetrieveException : Exception
+    {
+        public DataRetrieveException()
+        {
+        }
+
+        public DataRetrieveException(string? message) : base(message)
+        {
+        }
+
+        public DataRetrieveException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DataRetrieveException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
