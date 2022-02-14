43a8b88eae33ba51bf5906e38af7bc58efbe170f
Pierre-Marie <pm@cataire.fr>
2022-02-14 18:38:15 +0100

    Fix suite au merge #38 : DataManagers ajoutés en tant que services (#39)
    * (upd) suppression des using inutiles
    * (upd) ajout des IDataManager en tant que services
    * (upd) dans les vues "Asset" : DataManager appelés en tant que services

--- Fantastic3D/Fantastic3D.WPFApp/App.xaml.cs
+++ Fantastic3D/Fantastic3D.WPFApp/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Fantastic3D.AppModels;
+using Fantastic3D.DataManager;
+using Fantastic3D.Dto;
 using Fantastic3D.GUI.Utilities;
 using System;
 using System.Collections.Generic;
@@ -28,6 +30,13 @@ namespace Fantastic3D.GUI
             var configuration = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoToModelProfile)));
             var mapper = new Mapper(configuration);
             Services.RegisterService<IMapper>(mapper);
+
+            Services.RegisterService<IDataManager<User, UserDto>>(new ApiDataManager<User, UserDto>(client, mapper));
+            Services.RegisterService<IDataManager<Asset, AssetDto>>(new ApiDataManager<Asset, AssetDto>(client, mapper));
+            
+            // Services.RegisterService<IDataManager<Order, OrderDto>>(new ApiDataManager<Order, OrderDto>(client, mapper));
+            // Services.RegisterService<IDataManager<Review, ReviewDto>>(new ApiDataManager<Review, ReviewDto>(client, mapper));
+
         }
 
         
