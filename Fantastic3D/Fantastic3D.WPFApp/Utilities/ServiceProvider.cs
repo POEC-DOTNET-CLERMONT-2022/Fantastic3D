eefd84df1b69b2a82a82898fba7d97772e4b4e4a
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-14 15:42:51 +0100

    (add) ServiceProvider, classe centralisant les services nécessaires à l'application.

---
+++ Fantastic3D/Fantastic3D.WPFApp/Utilities/ServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantastic3D.GUI.Utilities
+{
+    public class ServiceProvider : IServiceProvider
+    {
+        private Dictionary<Type, object> _services = new();
+        public T GetService<T>() where T : class
+        {
+            return _services.GetValueOrDefault(typeof(T)) as T;
+        }
+
+        public void RegisterService<T>(T service) where T : class
+        {
+            _services.Add(typeof(T), service);
+        }
+    }
+}
