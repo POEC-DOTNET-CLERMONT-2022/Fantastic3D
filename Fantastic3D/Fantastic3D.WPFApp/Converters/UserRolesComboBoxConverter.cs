82e125b5a49a4a981e69dae38616491ae040d406
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-16 15:46:43 +0100

    (add) Converteur ComboBox des rôles vers Enum Role

---
+++ Fantastic3D/Fantastic3D.WPFApp/Converters/UserRolesComboBoxConverter.cs
@@ -0,0 +1,36 @@
+﻿using Fantastic3D.AppModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Fantastic3D.GUI.Converters
+{
+    [ValueConversion(typeof(UserRole), typeof(string))]
+    internal class UserRolesComboBoxConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is UserRole role)
+            {
+                return role.ToString();
+            }
+            return "Basic";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string roleAsString)
+            {
+                return roleAsString switch
+                {
+                    "Admin" => UserRole.Admin,
+                    "Premium" => UserRole.Premium,
+                    _ => UserRole.Basic,
+                };
+            }
+            return UserRole.Basic;
+        }
+    }
+}
