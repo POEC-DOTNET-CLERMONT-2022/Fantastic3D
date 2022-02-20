ffca5fc2ff15dbbf904b745088cf46157fcc791f
Sylvain125 <kenjii@hotmail.fr>
2022-02-20 14:07:05 +0100

    Ajout/Modification de listobservable pour order et review

--- Fantastic3D/Fantastic3D.AppModels/Order.cs
+++ Fantastic3D/Fantastic3D.AppModels/Order.cs
@@ -9,13 +9,13 @@ namespace Fantastic3D.AppModels
     {
 
         public int Id { get; set; }
-        public DateTime Date { get; private set; }
+        public DateTime Date { get;  set; }
 
-        public int PurchasesCount { get; private set; }
-        public int PurchasingUserId { get; private set; }
-        public string PurchasingUserName { get; private set; }
-        public int TotalPurchasedItems { get; private set; }
-        public float TotalPurchasePrice { get; private set; }
+        public int PurchasesCount { get;  set; }
+        public int PurchasingUserId { get;  set; }
+        public string PurchasingUserName { get;  set; }
+        public int TotalPurchasedItems { get;  set; }
+        public float TotalPurchasePrice { get;  set; }
 
 
         public override string ToString()
