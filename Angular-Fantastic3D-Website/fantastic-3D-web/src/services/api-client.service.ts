1163693b23a4f524f1dc596f1f132939e38993a0
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-21 16:29:59 +0100

    (Angular) update ApiClient

--- Angular-Fantastic3D-Website/fantastic-3D-web/src/services/api-client.service.ts
+++ Angular-Fantastic3D-Website/fantastic-3D-web/src/services/api-client.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from "@angular/common/http";
-import { User } from 'src/models/user';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { User } from 'src/models/user';
 import { IUserList } from 'src/models/i-user-list';
+import { IUser } from 'src/models/i-user';
 // import { userInfo } from 'os';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiClientService {
-
   baseUrl: string = 'https://localhost:7164/api/';
+  
   headers: {headers: HttpHeaders} = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/ld+json',
+      'Content-Type':  'application/json',
       Authorization: 'my-auth-token'
     })
   };
@@ -32,21 +33,22 @@ export class ApiClientService {
         role: user.role,
     }
     return this.httpClient.post<User>(
-      this.baseUrl + "user/",
+      this.baseUrl + "User/",
       body,
       this.headers);
   }
-  getUserById(id: number): Observable<User> {
-    return this.httpClient.get<User>(this.baseUrl + 'user/' + id);
+  getUserById(id: number): Observable<IUser> {
+    return this.httpClient.get<IUser>(this.baseUrl + 'User/' + id);
   }
 
   getUsersByOffset(offset: number, limit: number = 20): Observable<IUserList> {
-    return this.httpClient.get<IUserList>(this.baseUrl + 'user/');
+    return this.httpClient.get<IUserList>(this.baseUrl + 'User/');
+    //return this.httpClient.get<IUserList>(this.baseUrl + 'User');
     // TODO : ajouter une pagination, requêtes : 'user/?offset='+offset+'&limit='+limit);
   }
 
-  getUrlResult(url: string): Observable<User> {
-    return this.httpClient.get<User>(url);
+  getUrlResult(url: string): Observable<IUser> {
+    return this.httpClient.get<IUser>(url);
   }
 
 }
