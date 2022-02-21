17f7b012a0268013926c84f3257002ab646c9b4a
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-21 16:29:09 +0100

    (Angular) Update UserList

--- Angular-Fantastic3D-Website/fantastic-3D-web/src/app/user-list/user-list.component.ts
+++ Angular-Fantastic3D-Website/fantastic-3D-web/src/app/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserList } from 'src/models/i-user-list';
-import { User } from 'src/models/user';
+import { IUser } from 'src/models/i-user';
 import { ApiClientService } from 'src/services/api-client.service';
 
 @Component({
@@ -9,17 +9,20 @@ import { ApiClientService } from 'src/services/api-client.service';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  UserArray: User[] = [];
   iUserList: IUserList|undefined;
+  UserArray: IUser[] = [];
   offset: number = 0;
+  
   constructor(public apiService: ApiClientService) { }
 
   ngOnInit(): void {
+    
+    this.UserArray = [];
     this.apiService.getUsersByOffset(0)
     .subscribe((iUserList: IUserList) => {
       this.iUserList = iUserList;
       for(let result of iUserList.results) {
-        this.apiService.getUserById(result.id).subscribe((User: User) => {
+        this.apiService.getUserById(result.id).subscribe((User: IUser) => {
           this.UserArray.push(User);
           this.UserArray.sort((p1, p2) => p1.id - p2.id);
         });
