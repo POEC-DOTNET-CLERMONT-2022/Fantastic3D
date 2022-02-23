48318c040a9dafe1312e47cd5770ace5e4a90a48
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-23 20:25:50 +0100

    (Angular) Formulaire User OK CRUD

--- Angular-Fantastic3D-Website/fantastic-3D-web/src/app/user-form/user-form.component.ts
+++ Angular-Fantastic3D-Website/fantastic-3D-web/src/app/user-form/user-form.component.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from "src/models/user";
 import { ApiClientService } from 'src/services/api-client.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
 
-  user : User = new User();
-  constructor(private userService: ApiClientService) {
-    
+  user: User;
+  private readonly activatedRoute: ActivatedRoute;
+  private readonly userService: ApiClientService
+  constructor(activatedRoute: ActivatedRoute, userService: ApiClientService) {
+    this.activatedRoute = activatedRoute;
+    this.userService = userService;
+    this.user = new User();
+  }
+  roles = [
+    { id: 1, name: "Basic" },
+    { id: 2, name: "Premium" },
+    { id: 9, name: "Admin" }
+  ];
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((param) => {
+      this.userService.getUserById(param['userId']).subscribe((user: User) => {
+        this.user = user;
+      });
+    });
   }
 
   onSubmit(): void {
     console.log(this.user);
-    this.userService.postUser(this.user).subscribe( (newUser : User) => console.log(newUser));
+    if (this.user.id == 0) {
+      this.userService.postUser(this.user).subscribe((newUser: User) => console.log(newUser));
+    }
+    else {
+      this.userService.updateUserById(this.user.id, this.user).subscribe((newUser: User) => console.log(newUser));
+    }
+  }
+  onDelete(): void {
+    this.userService.deleteUserById(this.user.id);
   }
 
 }
+
