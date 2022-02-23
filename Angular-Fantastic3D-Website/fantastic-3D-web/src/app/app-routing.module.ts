33ffbbd14a424a62047abc874da488af3918906e
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-23 20:23:59 +0100

    (Angular) Routage des composants

--- Angular-Fantastic3D-Website/fantastic-3D-web/src/app/app-routing.module.ts
+++ Angular-Fantastic3D-Website/fantastic-3D-web/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { AssetListComponent } from './asset-list/asset-list.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: MainMenuComponent.pathUserList, component: UserListComponent },
+  { path: MainMenuComponent.pathUserNew, component: UserFormComponent },
+  { path: MainMenuComponent.pathUserForm, component: UserFormComponent },
+  { path: MainMenuComponent.pathUserForm + '/:userId', component: UserFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
