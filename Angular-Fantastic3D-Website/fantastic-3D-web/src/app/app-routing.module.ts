c2f08bcf12d729b7ebcd07fb926bcf9fc6f78b6f
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-26 12:28:52 +0100

    (Angular) Cleaned up routes

--- Angular-Fantastic3D-Website/fantastic-3D-web/src/app/app-routing.module.ts
+++ Angular-Fantastic3D-Website/fantastic-3D-web/src/app/app-routing.module.ts
@@ -6,10 +6,9 @@ import { AssetListComponent } from './asset-list/asset-list.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 
 const routes: Routes = [
-  { path: MainMenuComponent.pathUserList, component: UserListComponent },
+  { path: MainMenuComponent.pathUsers, component: UserListComponent },
+  { path: MainMenuComponent.pathUsers + '/:userId', component: UserFormComponent },
   { path: MainMenuComponent.pathUserNew, component: UserFormComponent },
-  { path: MainMenuComponent.pathUserForm, component: UserFormComponent },
-  { path: MainMenuComponent.pathUserForm + '/:userId', component: UserFormComponent },
 ];
 
 @NgModule({
