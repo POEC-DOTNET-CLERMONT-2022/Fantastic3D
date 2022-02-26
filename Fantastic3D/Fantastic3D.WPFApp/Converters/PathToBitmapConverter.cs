3a1727cf36bef0db68c6fa7b4a6ba22ac3a73c56
Pierre-Marie Aubineau <pm@cataire.fr>
2022-02-26 17:39:41 +0100

    (WPF / AssetView) Show notfound image when file not found (WPF / AssetView) Unpublished asset listed

--- Fantastic3D/Fantastic3D.WPFApp/Converters/PathToBitmapConverter.cs
+++ Fantastic3D/Fantastic3D.WPFApp/Converters/PathToBitmapConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,16 @@ namespace Fantastic3D.GUI.Converters
     [ValueConversion(typeof(string), typeof(Uri))]
     internal class PathToBitmapConverter : IValueConverter
     {
-        //private readonly string _picturePath = @"file:///D:/FormationCsharp/repository/Fantastic3D-Current/Fantastic3D/Data/pics/";
-        private readonly string _picturePath = @"Data/pics/";
+        private readonly string _picturePath = @"pack://application:,,,/../../../../Data/pics/";
+        //private readonly string _picturePath = @"Data/pics/";
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if(value is string path)
             {
-                return new Uri(_picturePath + path, UriKind.Relative);
+                if (File.Exists(_picturePath + path))
+                    return new Uri(_picturePath + path, UriKind.RelativeOrAbsolute);
             }
-            return new Uri(_picturePath + "notfound.png", UriKind.Relative);
+            return new Uri("/medias/notfound.png", UriKind.RelativeOrAbsolute);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
